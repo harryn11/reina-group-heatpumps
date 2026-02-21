0842376fb0478bbd6601407233a32a95923abcc5
HarryNorris <harry@auctaai.co.uk>
2026-02-21 21:38:57 +0000

    Change document title to dynamically load from siteData

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { siteData } from './config/siteData';
 import './index.css';
+
+document.title = siteData.company.name;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
